e9a922db7331b23c0c6e25e09a4862c3c79ce5a0
Parikshit <Parikshitmali17@gmail.com>
2025-04-21 15:34:48 +0530

    pp

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main" className="ns-wrapper"><h1 data-ns-test="project-name">Project Name</h1>
+    <h6 data-ns-test="project-description">Description</h6></div>
   )
 }
 
